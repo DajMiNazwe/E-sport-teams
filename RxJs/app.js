765ce39d73c7155c0463305f4421825c9707a906
Michal Gorski <dajminazwe@gmail.com>
2017-04-22 17:49:57 +0200

    Results are now visible as list

--- RxJs/app.js
+++ RxJs/app.js
@@ -1,16 +1,23 @@
 var q = $('#q');
 var keyups = Rx.Observable.fromEvent(q, 'keyup');
-keyups.throttleTime(500).map(function(x) {
+keyups.throttleTime(500).map((x) => {
         return q.val();
     })
-    .switchMap(function(x) {
+    .switchMap((x) => {
         return searchWikipedia(x)
     })
-    .do(function(x) {
+    .do((x) => {
         console.log(x);
     })
-    .subscribe(function(x) {
-        $('#results').text(x);
+    .subscribe((x) => {
+      $('li').remove();
+      if(x[1]) {
+        x[1].forEach((element) => {
+          $('#results > ul').append('<li>'+ element +'</li>');
+        })
+      } else {
+          $('#results').append('<h4>YOU NEED TO TYPE SOMETHING!</h4>');
+      }
     });
 
 function searchWikipedia(term) {
